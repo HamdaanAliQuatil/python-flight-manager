d769adb34ae9de751e4e1c98cfd250839cbf7570
HamdaanAliQuatil <96776914+HamdaanAliQuatil@users.noreply.github.com>
2022-08-16 21:36:44 +0530

    feat: add functions to extract aircraft model, number and airline

--- main.py
+++ main.py
@@ -12,5 +12,18 @@ class Flight:
         if not (number[2:].isdigit() and int(number[2:]) <= 9999):
             raise ValueError("Invalid route number '{}'".format(number))
 
+        self._number = number
+        self._aircraft = aircraft
+    
+    def aircraft_model(self):
+        return self._aircraft.model()
+    
+    def number(self):
+        return self._number
+
+    def airline(self):
+        return self._number[:2]
+
+
 if __name__ == '__main__':
 
